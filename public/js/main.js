61bcb4b0e287da65c914b726d083e7df4fa7ce8a
himanshu <work.himanshukaushuk@gmail.com>
2024-12-01 11:59:22 +0530

    updated

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
-import createElementFromHtml from "./js/createElementFromHtml.js";
-import products from "./js/productData.js";
+import createElementFromHtml from "./createElementFromHtml.js";
+import products from "./productData.js";
 const prod1 = products[0];
 export default function makeTable(myobject){
-    const table=document.querySelector('.main-table');
+    const table=document.querySelector('.header-tr');
         Object.keys(myobject).forEach((value) => {
-            table.appendChild(`<tr>${createElementFromHtml(`<td>${value}</td>`)}</tr>`)
+            let table2=table.appendChild(createElementFromHtml(`<td>${value}</td>`));
+            console.table(table2)
         });
 };
 makeTable(prod1);
