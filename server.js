1fff20eaa09e6a468803389ec9b9304603f29dc8
himanshu <work.himanshukaushuk@gmail.com>
2024-12-01 11:58:01 +0530

    modified server.js

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ import express from "express";
 import ejs from "ejs";
 import process from "process";
 import path from "path";
-import makeTable from "./public/js/main.js";
-import products from "./public/js/productData.js";
 const app=express();
 app.use(express.static(path.join(process.cwd(),'views')));
 app.use(express.static(path.join(process.cwd(),'public')));
+// app.use('content-type','text/html');
 //koi jaruruat nahi hai '/' ko folder ke aage add karne ke express apne aap kar dega
 app.set("view engine","ejs");
 app.set("views", path.join(process.cwd(),"views"));
@@ -15,7 +14,7 @@ app.get('/',function(req,res,next){
     res.send("welcome to home page.")
 });
 app.get('/products',function(req,res,next){
-    res.render("index.ejs");    
+    res.render("index");    
 })
 const PORT = 3000;
 app.listen(PORT, () => {
